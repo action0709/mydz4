d454536b4fb7f593bc345eb24dc01ea9b16c439b
action0709 <action0709@mail.ru>
2023-05-05 20:53:43 +0400

    1

---
+++ src/main/java/pro/sky/skyprospringmydz4/SkyproSpringMydz4Application.java
@@ -0,0 +1,13 @@
+package pro.sky.skyprospringmydz4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyproSpringMydz4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyproSpringMydz4Application.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/skyprospringmydz4/SkyproSpringMydz4ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.skyprospringmydz4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyproSpringMydz4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
